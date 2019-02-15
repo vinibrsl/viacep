580a4fa638bf7ed4e3ae8cf8fa4f51312594e11e
Vinicius Brasil <vnbrs@icloud.com>
2019-02-14 22:30:12 -0200

    Add mocks to specs

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -3,45 +3,49 @@ require 'spec_helper'
 RSpec.describe ViaCep::Address do
   describe '#initialize' do
     context 'when a valid CEP is passed' do
-      subject(:address) { ViaCep::Address.new('80210130') }
-
       it 'returns the Address' do
+        mock_viacep_success
+
+        address = ViaCep::Address.new('80210130')
         expect(address.address).not_to be_empty
       end
     end
 
     context 'when a invalid length CEP is passed' do
       it 'raises ArgumentError' do
-        expect { ViaCep::Address.new('000000') }.
-          to raise_error(ArgumentError)
+        expect { ViaCep::Address.new('000000') }
+          .to raise_error(ArgumentError)
       end
     end
 
     context 'when a unexistent CEP is passed' do
       it 'raises ViaCep::ApiRequestError' do
-        expect { ViaCep::Address.new('00000000') }.
-          to raise_error(ViaCep::ApiRequestError)
+        mock_viacep_not_found
+        expect { ViaCep::Address.new('00000000') }
+          .to raise_error(ViaCep::ApiRequestError)
       end
     end
 
     context 'when a nil CEP is passed' do
       it 'raises ArgumentError' do
-        expect { ViaCep::Address.new(nil) }.
-          to raise_error(ArgumentError)
+        expect { ViaCep::Address.new(nil) }
+          .to raise_error(ArgumentError)
       end
     end
 
     context 'when a low timeout is specified' do
       it 'raises a Timeout::Error' do
-        expect { ViaCep::Address.new('80210130', timeout: 0.001) }.
-          to raise_error(Timeout::Error)
+        mock_viacep_slow_success(delay_in_seconds: 0.01)
+        expect { ViaCep::Address.new('80210130', timeout: 0.001) }
+          .to raise_error(Timeout::Error)
       end
     end
 
     context 'when a regular timeout is specified' do
-      subject(:address) { ViaCep::Address.new('80210130', timeout: 25) }
-
       it 'returns the Address' do
+        mock_viacep_success
+
+        address = ViaCep::Address.new('80210130', timeout: 3.0)
         expect(address.address).not_to be_empty
       end
     end
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -8,23 +8,25 @@ RSpec.describe ViaCep::Service do
   describe '.fetch' do
     context 'when a small timeout is specified' do
       it 'raises Timeout::Error' do
-        expect { ViaCep::Service.fetch('80210130', 0.001) }.
-          to raise_error(Timeout::Error)
+        mock_viacep_slow_success(delay_in_seconds: 0.01)
+        expect { ViaCep::Service.fetch('80210130', 0.001) }
+          .to raise_error(Timeout::Error)
       end
     end
 
     context 'when a suficient timeout is specified' do
-      subject(:response) { ViaCep::Service.fetch('80210130', 25) }
-
       it 'returns the response' do
+        mock_viacep_success
+        response = ViaCep::Service.fetch('80210130', 5.0)
         expect(response).not_to be_empty
       end
     end
 
     context 'when a invalid CEP is passed' do
       it 'raises ViaCep::ApiRequestError' do
-        expect { ViaCep::Service.fetch('000000', nil) }.
-          to raise_error(ViaCep::ApiRequestError)
+        mock_viacep_invalid_format
+        expect { ViaCep::Service.fetch('000000', nil) }
+          .to raise_error(ViaCep::ApiRequestError)
       end
     end
   end
