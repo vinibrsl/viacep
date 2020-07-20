451ce81889e94728a2585a9f3dbec1995b25fc63
Vinicius Brasil <vini@hey.com>
2020-07-20 13:53:18 -0300

    Improve Address docs

--- lib/viacep/address.rb
+++ lib/viacep/address.rb
@@ -4,7 +4,27 @@ module ViaCep
   class Address
     attr_reader :cep, :address, :neighborhood, :city, :state, :ibge, :gia
 
-    # Initializes an instance of Address and fetches the CEP using the external API
+    #
+    # Initializes an instance of ViaCep::Address and fetches the CEP using the external API.
+    #
+    # @example Fetch a CEP
+    #   ViaCep::Address.new('80210130')
+    #   #=> #<ViaCep::Address:0x00007fe52a8a0568 @cep="80210-130", @address="Rua José Ananias Mauad", @neighborhood="Jardim Botânico", @city="Curitiba", @state="PR", @ibge="4106902", @gia="">
+    #
+    # @example Fetch a CEP with a formatted string
+    #   ViaCep::Address.new('80210-130')
+    #   #=> #<ViaCep::Address:0x00007fe52a8a0568 @cep="80210-130", @address="Rua José Ananias Mauad", @neighborhood="Jardim Botânico", @city="Curitiba", @state="PR", @ibge="4106902", @gia="">
+    #
+    # @param [String] cep The CEP to be fetched.
+    # @option options [Integer] :timeout The timeout in seconds to the request.
+    #
+    # @raise [ArgumentError] This is raised when CEP is nil.
+    # @raise [ArgumentError] This is raised when CEP format is invalid.
+    # @raise [ViaCep::ApiRequestError] This is raised when the external API is down or the CEP does not exist.
+    # @raise [Timeout::Error] This is raised when the timeout argument is specified and the request timed out.
+    #
+    # @return [ViaCep::Address]
+    #
     def initialize(cep, options = {})
       if cep.nil?
         raise ArgumentError, 'CEP cannot be nil'
